891d3a30dcba8abb6c7dc468d91819acf5ab50fd
Anandaptra <anndptra658@gmail.com>
2024-03-28 19:05:44 +0700

    assignment

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
-    
-    // You can handle form submission here, e.g., send data to server or display a confirmation message
-    // For demonstration, let's just log the form data
+    event.preventDefault(); 
     const formData = new FormData(this);
     const formDataObject = {};
     formData.forEach((value, key) => {
